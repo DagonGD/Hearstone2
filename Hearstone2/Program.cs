691e06ca4f0ea537749ad5d97bcfbdd938fb5af3
ant <iljinaa@gmailcom>
2014-10-21 17:33:37 +0300

    Demo card

--- Hearstone2/Program.cs
+++ Hearstone2/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hearstone2.Core.Cards.Neutral;
 
 namespace Hearstone2
 {
@@ -10,6 +6,7 @@ namespace Hearstone2
 	{
 		static void Main(string[] args)
 		{
+			var bluegillWarrior = new BluegillWarrior();
 		}
 	}
 }
