27ffdecf812e5d457fd419ebc6b3f02904d1896d
ant <iljinaa@gmailcom>
2014-10-27 15:49:12 +0200

    ManaCostCard

--- Hearstone2.Core/Cards/Card.cs
+++ Hearstone2.Core/Cards/Card.cs
@@ -1,34 +1,18 @@
-﻿using Hearstone2.Core.Classes;
-
-namespace Hearstone2.Core.Cards
+﻿namespace Hearstone2.Core.Cards
 {
-	public abstract class Card
+	/// <summary>
+	/// Card in hand or on table
+	/// </summary>
+	public abstract class Card : ManaCostCard
 	{
-		public abstract string Title { get; }
-		public abstract int ManaCost { get; }
-		public Hero Owner { get; set; }
-
 		/// <summary>
 		/// To play any card we need to spend mana and remove this card from hand
 		/// </summary>
-		public virtual void Play()
+		public override void Play()
 		{
-			if (Owner.Mana < ManaCost)
-			{
-				throw new NotEnoughManaException();
-			}
-
-			Owner.Mana -= ManaCost;
 			Owner.Hand.Remove(this);
-		}
 
-		/// <summary>
-		/// To play some abstract card you need to have enoght mana
-		/// </summary>
-		/// <returns></returns>
-		public virtual bool CanPlay()
-		{
-			return Owner.Mana >= ManaCost;
+			base.Play();
 		}
 	}
 }
---
+++ Hearstone2.Core/Cards/ManaCostCard.cs
@@ -0,0 +1,33 @@
+﻿using Hearstone2.Core.Classes;
+
+namespace Hearstone2.Core.Cards
+{
+	public abstract class ManaCostCard
+	{
+		public abstract string Title { get; }
+		public abstract int ManaCost { get; }
+		public Hero Owner { get; set; }
+
+		/// <summary>
+		/// To play any card we need to spend mana
+		/// </summary>
+		public virtual void Play()
+		{
+			if (Owner.Mana < ManaCost)
+			{
+				throw new NotEnoughManaException();
+			}
+
+			Owner.Mana -= ManaCost;
+		}
+
+		/// <summary>
+		/// To play some abstract card you need to have enough mana
+		/// </summary>
+		/// <returns></returns>
+		public virtual bool CanPlay()
+		{
+			return Owner.Mana >= ManaCost;
+		}
+	}
+}
